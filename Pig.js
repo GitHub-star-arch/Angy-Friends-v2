1ac79b1d97bba5267236582d8ce899153cbc352d
GitHub-star-arch <nithinraggov@gmail.com>
2020-06-04 04:16:55 -0400

    hah

--- Pig.js
+++ Pig.js
@@ -1,7 +1,7 @@
 class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/enemy.png");
+    this.image = loadImage("Pictures/Harini_Pig.png");
     this.Visiblity = 255;
   }
 
